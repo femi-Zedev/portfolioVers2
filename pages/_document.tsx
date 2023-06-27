47a871df224dfcfde85799d48df8d2df05004b91
arnaudakotonou97 <akotonouarnaudfemi@gmail.com>
2023-06-27 20:57:51 +0100

    fix: fixing meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,19 +8,19 @@ export default function Document() {
         <meta charSet="utf-8"/>
         <meta name="google-site-verification" content="XjjGg1wH-mQBt186CmOXy-kpQGFhwAF2bLsqtMq3eps" />
         <meta name="robots" content="index,follow" />
-        <meta name="description" content="Hey, I'm Femi AKOTONOU. I'm a young frontend developper with over 4 years of experience by now.  I've spent the last few years perfecting my knowledge of Javascript-based stacks and building pretty cool solutions for companies as well as individuals." />
+        <meta name="description" content="Hey, I'm Femi AKOTONOU. I'm a Frontend Developer and a UX Designer" />
         <meta name="twitter:card" content="summary_large_image"/>
         <meta name="twitter:site" content="@FemiAkt"/>
         <meta name="twitter:creator" content="@FemiAkt"/>
         <meta property="og:title" content="Femi AKOTONOU"/>
-        <meta property="og:description" content="Hey, I'm Femi AKOTONOU. I'm a young frontend developper with over 4 years of experience by now.  I've spent the last few years perfecting my knowledge of Javascript-based stacks and building pretty cool solutions for companies as well as individuals."/>
-        <meta property="og:url" content="https://femidev.com/"/>
+        <meta property="og:description" content="Hey, I'm Femi AKOTONOU. I'm a Frontend Developer and a UX Designer"/>
+        <meta property="og:url" content="https://femidev.com"/>
         <meta property="og:image" content="https://femidev.com/femi.PNG"/>
         <meta property="og:image:alt" content="Femi Dev"/>
         <meta property="og:image:secure_url" content="https://femidev.com/femi.PNG"/>
         <meta property="og:image:type" content="image/jpeg"/>
         <meta property="og:site_name" content="Femi AKOTONOU | FrontEnd Engineer"/>
-        <link rel="canonical" href="https://femidev.com/"/>
+        <link rel="canonical" href="https://femidev.com"/>
       </Head>
       <body>
         <Main />
