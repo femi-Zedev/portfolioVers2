84a0a4f9787aca3bc7fd42646c3e895381632389
arnaudakotonou97 <akotonouarnaudfemi@gmail.com>
2024-04-20 17:40:44 +0100

    wip: fixing build error
    -removed api folder from pages/ and dead code from _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import { useEffect, useRef } from "react";
-import { RecoilRoot, atom, useRecoilState } from 'recoil';
+import { RecoilRoot } from 'recoil';
 
 
 
 
 export default function App({ Component, pageProps }: AppProps) {
-
-  const router = useRouter()
-  const locale = router.locale || router.defaultLocale!
-  const firstRender = useRef(true)
-
 
 
   return (
