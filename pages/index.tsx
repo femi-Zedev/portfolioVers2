32448152b94b60e36196cecfe2c460a88301786b
arnaudakotonou97 <akotonouarnaudfemi@gmail.com>
2024-04-20 17:49:26 +0100

    wip: fixing build error
    -checking build error from Home components

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,22 +5,25 @@ import { getHomePage } from "@/services/index"
 import { Page } from "@/interfaces/hygraph.interface";
 
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
 
-  const { pages } = await getHomePage()
+//   const { pages } = await getHomePage()
   
-  return {
-    props: {
-      pages
-    }
-  }
+//   return {
+//     props: {
+//       pages
+//     }
+//   }
 
-}
+// }
 
 
 
-const Index = ({pages}:{pages: Page[]}) => {
- const pageData = pages[0]
+// const Index = ({pages}:{pages: Page[]}) => {
+//  const pageData = pages[0]
+
+const Index = () => {
+ 
 
   return (
     <>
@@ -29,8 +32,8 @@ const Index = ({pages}:{pages: Page[]}) => {
         <meta charSet="utf-8"/>
         <meta name="google-site-verification" content="XjjGg1wH-mQBt186CmOXy-kpQGFhwAF2bLsqtMq3eps" />
         <meta name="robots" content="index,follow" />
-        <meta name="description" content={pageData.meta.desc} />
-        <meta property="og:description" content={pageData.meta.desc}/>
+        {/* <meta name="description" content={pageData.meta.desc} />
+        <meta property="og:description" content={pageData.meta.desc}/> */}
         <meta name="twitter:card" content="summary_large_image"/>
         <meta name="twitter:site" content="@FemiAkt"/>
         <meta name="twitter:creator" content="@FemiAkt"/>
@@ -40,10 +43,11 @@ const Index = ({pages}:{pages: Page[]}) => {
         <meta property="og:image:alt" content="Femi Dev"/>
         <meta property="og:image:secure_url" content="https://femidev.com/femi.PNG"/>
         <meta property="og:image:type" content="image/jpeg"/>
-        <meta property="og:site_name" content={pageData.meta.ogSiteName}/>
+        {/* <meta property="og:site_name" content={pageData.meta.ogSiteName}/> */}
         <link rel="canonical" href="https://femidev.com"/>
       </Head>
-      <Home pageData={pages[0]} />
+      {/* <Home pageData={pages[0]} /> */}
+      <div></div>
     </>
   );
 }
