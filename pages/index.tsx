9cb38260d37bb14dcca3eee7b9158c160024a56c
arnaudakotonou97 <akotonouarnaudfemi@gmail.com>
2024-04-20 18:12:59 +0100

    wip: fixing build error attempt 2
    -checking build error from getStatic props function

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,26 +4,23 @@ import Head from "next/head";
 import { getHomePage } from "@/services/index"
 import { Page } from "@/interfaces/hygraph.interface";
 
+interface Props {
+  pages: Page[];
+}
 
-// export async function getStaticProps() {
+export const getStaticProps = async () => {
+  const { pages } = await getHomePage();
 
-//   const { pages } = await getHomePage()
-  
-//   return {
-//     props: {
-//       pages
-//     }
-//   }
-
-// }
+  return {
+    props: {
+      pages,
+    },
+  };
+};
 
 
-
-// const Index = ({pages}:{pages: Page[]}) => {
-//  const pageData = pages[0]
-
-const Index = () => {
- 
+const Index: NextPage<Props> = ({ pages }) => {
+  const pageData = pages[0];
 
   return (
     <>
@@ -32,8 +29,8 @@ const Index = () => {
         <meta charSet="utf-8"/>
         <meta name="google-site-verification" content="XjjGg1wH-mQBt186CmOXy-kpQGFhwAF2bLsqtMq3eps" />
         <meta name="robots" content="index,follow" />
-        {/* <meta name="description" content={pageData.meta.desc} />
-        <meta property="og:description" content={pageData.meta.desc}/> */}
+        <meta name="description" content={pageData.meta.desc} />
+        <meta property="og:description" content={pageData.meta.desc}/>
         <meta name="twitter:card" content="summary_large_image"/>
         <meta name="twitter:site" content="@FemiAkt"/>
         <meta name="twitter:creator" content="@FemiAkt"/>
@@ -43,10 +40,10 @@ const Index = () => {
         <meta property="og:image:alt" content="Femi Dev"/>
         <meta property="og:image:secure_url" content="https://femidev.com/femi.PNG"/>
         <meta property="og:image:type" content="image/jpeg"/>
-        {/* <meta property="og:site_name" content={pageData.meta.ogSiteName}/> */}
+        <meta property="og:site_name" content={pageData.meta.ogSiteName}/>
         <link rel="canonical" href="https://femidev.com"/>
       </Head>
-      {/* <Home pageData={pages[0]} /> */}
+      <Home pageData={pages[0]} />
       <div></div>
     </>
   );
